8db03aabc7834bfd7afbcafd199f0ba5f3d0e9c4
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-03-06 13:36:11 +0900

    fix: add LogLevelEnum to module exports

--- src/beans_logging/__init__.py
+++ src/beans_logging/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from .__version__ import __version__
+from .constants import LogLevelEnum
 from .schemas import LoguruHandlerPM, LogHandlerPM
 from .config import get_default_handlers, LoggerConfigPM
 from ._core import Logger, logger, LoggerLoader
@@ -8,6 +9,7 @@ from .mode import log_at
 
 __all__ = [
     "__version__",
+    "LogLevelEnum",
     "LoguruHandlerPM",
     "LogHandlerPM",
     "get_default_handlers",
